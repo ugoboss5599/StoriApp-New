56d9fe94369645e7e411e1d1a43ac5299d974536
ugoboss <ugurcan@ulooksogood.com>
2024-08-20 15:45:08 +0300

    Initial Commit

---
+++ StoriApp New/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StoriApp New
+//
+//  Created by Ugurcan Sevindik on 20.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StoriApp New/StoriApp_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriApp_NewApp.swift
+//  StoriApp New
+//
+//  Created by Ugurcan Sevindik on 20.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct StoriApp_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
